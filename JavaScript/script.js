b40b95cdca1e9dfb4ee56a35a79f632b8763c499
xuefu <xuefu.sh@gmail.com>
2013-12-27 13:45:08 +0800

    add JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,39 @@
+"xuefu".length * 9
+
+confirm("Hello World!");
+
+prompt("What's your name?");
+
+
+// Check if the user is ready to play!
+// console.log("Snow White and Batman were hanging out at the bus stop, waiting to go to the shops. There was a sale on and both needed some new threads. You've never really liked Batman. You walk up to him.");
+// confirm("I am ready play!");
+// var age = prompt("how old are you?");
+// if(age > 18)
+// {
+//     console.log("Come on, baby...");
+//     }
+//     else
+//     {
+//         console.log("You are too young to play this game!");
+//         }
+//
+//         console.log("Batman glares at you.");
+//         var userAnswer = prompt("Are you feeling lucky, punk?");
+//         if(userAnswer === "yes")
+//         {
+//             console.log("Batman hits you very hard. It's Batman and you're you! Of course Batman wins!");
+//             }
+//             else
+//             {
+//                 console.log("You did not say yes to feeling lucky. Good choice! You are a winner in the game of not getting beaten up by Batman.");
+//                 }
+//                 var feedback = prompt("What's rate of this intresting game, you think?");
+//                 if(feedback > 8)
+//                 {
+//                     console.log("This is just the beginning of my game empire. Stay tuned for more!");
+//                     }
+//                     else
+//                     {
+//                         console.log("I slaved away at this game and you gave me that score?! The nerve! Just you wait!");
+//                         }
